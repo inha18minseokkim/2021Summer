7e2434d684403009f73d11a6ad6ecd378f5513ad
kimurloc <andynamgoo@gmail.com>
2021-07-20 15:53:16 +0900

    asdf

--- app/src/main/java/com/example/a2021summer/OrderActivity.kt
+++ app/src/main/java/com/example/a2021summer/OrderActivity.kt
@@ -1,4 +1,12 @@
 package com.example.a2021summer
 
-class OrderActivity {
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.a2021summer.databinding.ActivitySubBinding
+
+class OrderActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?){
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_order)
+    }
 }
